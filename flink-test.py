77f5b3c4924e515d1c7d13cc795ff0c4f7ceb93d
Till Krischer <till@krischer.io>
2020-03-13 14:01:35 +0100

    draw some graphs

--- flink-test.py
+++ flink-test.py
@@ -2,20 +2,62 @@ import numpy as np
 import time
 import urllib.request
 import urllib.parse
+import json
+import matplotlib.pyplot as plt
+
+jobmanagermetrics = [
+    "Status.JVM.Memory.Mapped.TotalCapacity",
+    "taskSlotsAvailable",
+    "Status.JVM.Memory.Mapped.MemoryUsed",
+    "taskSlotsTotal",
+    "Status.JVM.CPU.Time",
+    "Status.JVM.Threads.Count",
+    "Status.JVM.Memory.Heap.Committed",
+    "Status.JVM.Memory.Direct.MemoryUsed",
+    "numRunningJobs",
+    "Status.JVM.Memory.Mapped.Count",
+    "Status.JVM.CPU.Load",
+    "Status.JVM.Memory.Heap.Max",
+    "Status.JVM.Memory.Heap.Used"
+]
+taskmanagermetrics = [
+    "Status.Network.AvailableMemorySegments",
+    "Status.JVM.Memory.Mapped.TotalCapacity",
+    "Status.Network.TotalMemorySegments",
+    "Status.JVM.Memory.Mapped.MemoryUsed",
+    "Status.JVM.CPU.Time",
+    "Status.JVM.Threads.Count",
+    "Status.JVM.Memory.Heap.Committed",
+    "Status.JVM.Memory.Direct.Count",
+    "Status.JVM.Memory.NonHeap.Max",
+    "Status.JVM.Memory.NonHeap.Committed",
+    "Status.JVM.Memory.NonHeap.Used",
+    "Status.JVM.Memory.Direct.MemoryUsed",
+    "Status.JVM.Memory.Direct.TotalCapacity",
+    "Status.JVM.Memory.Mapped.Count",
+    "Status.JVM.CPU.Load",
+    "Status.JVM.Memory.Heap.Max",
+    "Status.JVM.Memory.Heap.Used"
+]
 
 
 def parseinput(filename):
-    with open('data.csv') as f:
+    with open(filename) as f:
         # skip first line
         f.readline()
 
         events = []
+        tsx = []
+        tsy = []
         line = f.readline()
         while line:
             time, value = line.split(",")
             time = float(time)
             # round down the value
             value = int(float(value) // 1)
+
+            tsx += [time]
+            tsy += [value]
 
             # generate value many events using a normal distribution around
             # the mean given by time
@@ -26,11 +68,11 @@ def parseinput(filename):
             line = f.readline()
 
         events.sort()
-        return events
+        return events, tsx, tsy
 
 
 def submitjob(jobarg):
-    jobid = "fc0ca690-2780-4922-b1c6-5d9a263b263a_piestimation-1.0-SNAPSHOT.jar"
+    jobid = "96e38cda-179a-4b5a-a6c1-7106137f73db_piestimation-1.0-SNAPSHOT.jar"
     url = f'http://localhost:8081/jars/{jobid}/run?programArg={jobarg}'
 
     req = urllib.request.Request(url, method="POST")
@@ -39,50 +81,51 @@ def submitjob(jobarg):
 
 
 def querymetrics():
-    jobmanagerurl = "http://localhost:8081/jobmanager/metrics?get=taskSlotsAvailable,Status.JVM.CPU.Time"
-    taskmanagerurl = "http://localhost:8081/taskmanagers/metrics?get=Status.JVM.CPU.Load"
+    jobmanagerurl = f"http://localhost:8081/jobmanager/metrics?get={','.join(jobmanagermetrics)}"
+    taskmanagerurl = f"http://localhost:8081/taskmanagers/metrics?get={','.join(taskmanagermetrics)}"
 
     metrics = {}
 
     with urllib.request.urlopen(jobmanagerurl) as f:
-        metrics["jobsmanager"] = f.read().decode()
+        metrics["jobsmanager"] = json.loads(f.read().decode())
 
     with urllib.request.urlopen(taskmanagerurl) as f:
-        metrics["taskmanager"] = f.read().decode()
+        metrics["taskmanager"] = json.loads(f.read().decode())
 
     return metrics
 
 
 dataA = "dataA.csv"
-dataB = "dataB.csv"
-dataA_weight = 100000
+dataB = "dataC.csv"
+# dataA_weight = 100000
+dataA_weight = 1000000
 dataB_weight = 300000
 
-eventsA = parseinput(dataA)
-eventsB = parseinput(dataB)
+eventsA, tsxA, tsyA = parseinput(dataA)
+eventsB, tsxB, tsyB = parseinput(dataB)
 
 start = time.time()
 lastsecond = 0
 
 metrics = {}
 
-while len(eventsA) > 0:
+while len(eventsA) > 0 or len(eventsB) > 0:
     elapsedtime = time.time() - start
 
     # A
-    if elapsedtime > eventsA[0]:
+    if len(eventsA) > 0 and elapsedtime > eventsA[0]:
         print("submitting job type A")
         submitjob(dataA_weight)
         eventsA.pop(0)
 
     # B
-    if elapsedtime > eventsB[0]:
-        print("submitting job type A")
+    if len(eventsB) > 0 and elapsedtime > eventsB[0]:
+        print("submitting job type B")
         submitjob(dataB_weight)
         eventsB.pop(0)
 
     # metrics
-    elapsedseconds = elapsedtime // 1
+    elapsedseconds = int(elapsedtime)
     if elapsedseconds > lastsecond:
         print(elapsedseconds)
         m = querymetrics()
@@ -91,4 +134,54 @@ while len(eventsA) > 0:
 
     time.sleep(0.01)
 
-print(metrics)
+jmmet = {}
+tmmet = {}
+for met in jobmanagermetrics:
+    jmmet[met] = []
+
+for met in taskmanagermetrics:
+    tmmet[met] = []
+
+i = 1
+while i in metrics:
+    for o in metrics[i]["jobsmanager"]:
+        value = ""
+        if "value" in o:
+            value = o["value"]
+        elif "avg" in o:
+            value = o["avg"]
+        jmmet[o["id"]] += [float(value)]
+    for o in metrics[i]["taskmanager"]:
+        value = ""
+        if "value" in o:
+            value = o["value"]
+        elif "avg" in o:
+            value = o["avg"]
+        tmmet[o["id"]] += [float(value)]
+    i += 1
+
+
+def showplot(tsxA, tsyA, metrics, name):
+    _, ax1 = plt.subplots()
+    # ax1
+    color = 'tab:blue'
+    ax1.set_xlabel('time (s)')
+    ax1.set_ylabel('generated', color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
+    ax1.plot(tsxA, tsyA, color=color)
+    # ax1.plot(tsxB, tsyB, color=color)
+
+    # ax2
+    ax2 = ax1.twinx()
+    color = 'tab:red'
+    ax2.set_ylabel(name, color=color)
+    ax2.plot(metrics, color=color)
+    ax2.tick_params(axis='y', labelcolor=color)
+    plt.show()
+
+
+for met in jmmet:
+    showplot(tsxA, tsyA, jmmet[met], met)
+
+for met in tmmet:
+    showplot(tsxA, tsyA, tmmet[met], met)
